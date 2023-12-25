e2b1edbea5c9fbae1135bebc482ae5d5a563fccd
Artur Yumaev <art.yumaev@gmail.com>
2023-12-25 15:13:48 +0300

    Added example for wild pointers

--- arenas/2-wild-pointer/main.go
+++ arenas/2-wild-pointer/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"arena"
+	"fmt"
+	"unsafe"
+)
+
+func write() {
+	mem := arena.NewArena()
+
+        s := arena.MakeSlice[int](mem, 10, 10)
+        fmt.Printf("array ptr: %p\n", unsafe.Pointer(&s[0]))
+
+        mem.Free()
+        
+	s[0] = 42
+}
+
+func read() {
+        mem := arena.NewArena()
+
+        s := arena.MakeSlice[int](mem, 10, 10)
+        fmt.Printf("array ptr: %p\n", unsafe.Pointer(&s[0]))
+
+        mem.Free()
+        
+	s = append(s, 42)
+}
+
 func main() {
-	return
+	// write()
+	read()
 }
