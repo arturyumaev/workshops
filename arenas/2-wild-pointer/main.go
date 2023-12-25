ddecfeaca6d2107194f7d2fb981f4ab4d7b8dd88
Artur Yumaev <art.yumaev@gmail.com>
2023-12-25 15:18:31 +0300

    Added wild pointers exaple

--- arenas/2-wild-pointer/main.go
+++ arenas/2-wild-pointer/main.go
@@ -9,22 +9,22 @@ import (
 func write() {
 	mem := arena.NewArena()
 
-        s := arena.MakeSlice[int](mem, 10, 10)
-        fmt.Printf("array ptr: %p\n", unsafe.Pointer(&s[0]))
+	s := arena.MakeSlice[int](mem, 10, 10)
+	fmt.Printf("array ptr: %p\n", unsafe.Pointer(&s[0]))
 
-        mem.Free()
-        
+	mem.Free()
+
 	s[0] = 42
 }
 
 func read() {
-        mem := arena.NewArena()
+	mem := arena.NewArena()
 
-        s := arena.MakeSlice[int](mem, 10, 10)
-        fmt.Printf("array ptr: %p\n", unsafe.Pointer(&s[0]))
+	s := arena.MakeSlice[int](mem, 10, 10)
+	fmt.Printf("array ptr: %p\n", unsafe.Pointer(&s[0]))
 
-        mem.Free()
-        
+	mem.Free()
+
 	s = append(s, 42)
 }
 
